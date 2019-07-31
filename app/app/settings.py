9019fecb82641e82a9c3634f2ca8dc37132ba9b8
rm031015 <rm031015@gmail.com>
2019-07-31 10:53:17 +0800

    Added super user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default':{
+
+            'ENGINE': 'sql_server.pyodbc',
+            'NAME': 'testapi',
+            'USER':'sa',
+            'PASSWORD':'sa',
+            'HOST':'127.0.0.1',
+            'PORT':'',
+            'OPTIONS':{
+                'driver':'ODBC Driver 17 for SQL Server',
+            }
 }
 
 
